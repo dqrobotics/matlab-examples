4d03a1ce2f38f6073062e9494842759000410920
Juan Jose Quiroz <juanjqogm@gmail.com>
2023-07-12 12:49:47 +0900

    [constrained_kinematic_control] Added modifications to comply the Bruno's suggestions
    - Moved outside the try-catch block lines that are independent on CoppeliaSim.
    - Added comments to explain the need for lines related to some CoppeliaSim commands. Ex: trigger_next_simulation_step, set_joint_target_velocities, set_object_pose.
    - Added a new variable to describe better why I'm using 0 instead of plane_d in the line controller/compute_setpoint_control_signal.

--- vrep/constrained_kinematic_control/constrained_kinematic_control.m
+++ vrep/constrained_kinematic_control/constrained_kinematic_control.m
@@ -31,34 +31,35 @@ vi.connect('127.0.0.1',19997);
 vi.set_synchronous(true);
 vi.start_simulation();
 
-try
-
 jointnames={'LBR4p_joint1','LBR4p_joint2',...
             'LBR4p_joint3','LBR4p_joint4',...
             'LBR4p_joint5','LBR4p_joint6',...
             'LBR4p_joint7'};
 
-    %---------------------Robot definition--------------------------%
-    LBR4p_DH_theta = [0, 0, 0, 0, 0, 0, 0];
-    LBR4p_DH_d = [0.200, 0, 0.4, 0, 0.39, 0, 0];
-    LBR4p_DH_a = [0, 0, 0, 0, 0, 0, 0];
-    LBR4p_DH_alpha = [pi/2, -pi/2, pi/2, -pi/2, pi/2, -pi/2, 0];
-    LBR4p_DH_type = double(repmat(DQ_JointType.REVOLUTE,1,7));
-    LBR4p_DH_matrix = [LBR4p_DH_theta;
-                        LBR4p_DH_d;
-                        LBR4p_DH_a;
-                        LBR4p_DH_alpha
-                        LBR4p_DH_type];
-                
-    robot = DQ_SerialManipulatorDH(LBR4p_DH_matrix);
+%---------------------Robot definition--------------------------%
+LBR4p_DH_theta = [0, 0, 0, 0, 0, 0, 0];
+LBR4p_DH_d = [0.200, 0, 0.4, 0, 0.39, 0, 0];
+LBR4p_DH_a = [0, 0, 0, 0, 0, 0, 0];
+LBR4p_DH_alpha = [pi/2, -pi/2, pi/2, -pi/2, pi/2, -pi/2, 0];
+LBR4p_DH_type = double(repmat(DQ_JointType.REVOLUTE,1,7));
+LBR4p_DH_matrix = [LBR4p_DH_theta;
+                    LBR4p_DH_d;
+                    LBR4p_DH_a;
+                    LBR4p_DH_alpha
+                    LBR4p_DH_type];
+
+robot = DQ_SerialManipulatorDH(LBR4p_DH_matrix);
+robot.set_effector(1+0.5*DQ.E*DQ.k*0.07);
+
+n = robot.get_dim_configuration_space();
+qmin = [-pi -pi -pi/2 -pi -pi/2 -pi -pi/2]';
+qmax = [pi pi pi/2 pi pi/2 pi pi/2]';
+
+try           
     xbase = vi.get_object_pose('LBR4p_joint1');
     robot.set_reference_frame(xbase);
     robot.set_base_frame(xbase);
-    robot.set_effector(1+0.5*DQ.E*DQ.k*0.07);
-    xbase = robot.get_base_frame();
-    n = robot.get_dim_configuration_space();
-    qmin = [-pi -pi -pi/2 -pi -pi/2 -pi -pi/2]';
-    qmax = [pi pi pi/2 pi pi/2 pi pi/2]';
+    vi.set_object_pose('base', xbase);
     %----------------------------------------------------------------
     %---------------------Controller definition----------------------%
     qp_solver = DQ_QuadprogSolver();
@@ -71,6 +72,7 @@ jointnames={'LBR4p_joint1','LBR4p_joint2',...
     plane_d = Adsharp(xplane,-k_);
     controller.set_target_primitive(plane_d);
     controller.set_stability_threshold(0.00001);
+    desired_distance_to_target_plane = 0;
     %----------------------------------------------------------------
     %  Plane constraints
     safe_distance = 0.1;
@@ -78,13 +80,16 @@ jointnames={'LBR4p_joint1','LBR4p_joint2',...
     floor_plane = Adsharp(vi.get_object_pose('floor_plane'), k_);
     back_plane  = Adsharp(vi.get_object_pose('back_plane'),  k_);
     planes = {wall_plane, floor_plane, back_plane};
-
-    vi.trigger_next_simulation_step();
-    q =  vi.get_joint_positions(jointnames);
     %----------------------------------------------------------------
+
     j=1;
     while ~controller.system_reached_stable_region()
-        %----------VFIs------------------------------------
+        %---------------------------------------------------------------%
+        % This command performs a simulation step in CoppeliaSim. This step
+        % is required when you are working in synchronous mode.
+        vi.trigger_next_simulation_step();
+        %---------------------------------------------------------------%
+
         q =  vi.get_joint_positions(jointnames);
         x_pose = robot.fkm(q);
         p = translation(x_pose); 
@@ -99,14 +104,25 @@ jointnames={'LBR4p_joint1','LBR4p_joint2',...
         A = [-Jdists; -eye(n); eye(n)];
         b = [dists'; (q-qmin); -(q-qmax)];
         controller.set_inequality_constraint(A,b);
-        %-------------------------------------------------
-        u = controller.compute_setpoint_control_signal(q, 0);
+        u = controller.compute_setpoint_control_signal(q, ...
+                                        desired_distance_to_target_plane);
+
+        %---------------------------------------------------------------%
+        % Set the target joint velocities in CoppeliaSim. This command is
+        % required when you are working in synchronous mode and the robot 
+        % joints are set in velocity control mode.
         vi.set_joint_target_velocities(jointnames, u);
+        %---------------------------------------------------------------%
+
+        %---------------------------------------------------------------%
+        %----Set the effector frame object on CoppeliaSim to check if
+        %----the kinematic model is matching. This step is optional.
         vi.set_object_pose('effector', robot.fkm(q))
-        vi.set_object_pose('base', xbase);
+        %---------------------------------------------------------------%
+        
         norm(controller.get_last_error_signal())
-        vi.trigger_next_simulation_step();
-        %----------------
+
+
         u_log(:,j) = u;
         q_dot(:,j) = vi.get_joint_velocities(jointnames);
         j = j+1;
