3ead8343be8005a05d281b4012718dd3d1012916
Frederico Fernandes Afonso Silva <frederico.fernandes.afonso@gmail.com>
2024-08-15 17:31:35 +0100

    [lbr4_torque_control.m] Added copyright.

--- vrep/lbr4-torque-control/lbr4_torque_control.m
+++ vrep/lbr4-torque-control/lbr4_torque_control.m
@@ -9,6 +9,29 @@
 %           Available at: https://osf.io/gmhba/?view_only=e0122282979d43af93aa280cfd390df3
 %       2) Run this file.
 
+% (C) Copyright 2011-2024 DQ Robotics Developers
+% 
+% This file is part of DQ Robotics.
+% 
+%     DQ Robotics is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU Lesser General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     DQ Robotics is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU Lesser General Public License for more details.
+% 
+%     You should have received a copy of the GNU Lesser General Public License
+%     along with DQ Robotics.  If not, see <http://www.gnu.org/licenses/>.
+%
+% DQ Robotics website: dqrobotics.github.io
+%
+% Contributors to this file:
+%     1.  Frederico Fernandes Afonso Silva (frederico.silva@ieee.org)
+%        - Responsible for the original implementation.
+
 close all
 clear class
 clear all %#ok
